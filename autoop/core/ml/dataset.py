46f0240a5cff7f850cf2d5827d35bf08c602ba35
matei <kietamk11@gmail.com>
2024-11-06 18:23:03 +0100

    suggested changes from TAs

--- autoop/core/ml/dataset.py
+++ autoop/core/ml/dataset.py
@@ -3,13 +3,16 @@ from abc import ABC, abstractmethod
 import pandas as pd
 import io
 
-class Dataset(Artifact):
 
+class Dataset(Artifact):
+    """A class to represent an ML dataset"""
     def __init__(self, *args, **kwargs):
         super().__init__(type="dataset", *args, **kwargs)
 
     @staticmethod
-    def from_dataframe(data: pd.DataFrame, name: str, asset_path: str, version: str="1.0.0"):
+    def from_dataframe(data: pd.DataFrame, name: str,
+                       asset_path: str, version: str = "1.0.0"):
+        """ Create a dataset from a pandas dataframe."""
         return Dataset(
             name=name,
             asset_path=asset_path,
@@ -18,11 +21,12 @@ class Dataset(Artifact):
         )
 
     def read(self) -> pd.DataFrame:
+        """ Read data from a given path """
         bytes = super().read()
         csv = bytes.decode()
         return pd.read_csv(io.StringIO(csv))
 
     def save(self, data: pd.DataFrame) -> bytes:
+        """ Save data to a given path """
         bytes = data.to_csv(index=False).encode()
         return super().save(bytes)
-    
--- autoop/functional/feature.py
+++ autoop/functional/feature.py
@@ -1,7 +1,7 @@
-
 from typing import List
 from autoop.core.ml.dataset import Dataset
 from autoop.core.ml.feature import Feature
+
 
 def detect_feature_types(dataset: Dataset) -> List[Feature]:
     """Assumption: only categorical and numerical features and no NaN values.
@@ -20,7 +20,6 @@ def detect_feature_types(dataset: Dataset) -> List[Feature]:
             type = "categorical"
         else:
             type = "numerical"
-        features.append(Feature(name = name, type = type))
+        features.append(Feature(name=name, type=type))
 
     return features
-
