6bfb1512cd4bead7d68f8222d36d12a3408e7290
matei <kietamk11@gmail.com>
2024-11-09 19:57:13 +0100

    fixed tests

--- autoop/tests/test_pipeline.py
+++ autoop/tests/test_pipeline.py
@@ -6,7 +6,9 @@ from autoop.core.ml.pipeline import Pipeline
 from autoop.core.ml.dataset import Dataset
 from autoop.core.ml.feature import Feature
 from autoop.functional.feature import detect_feature_types
-from autoop.core.ml.model.regression import MultipleLinearRegression
+from autoop.core.ml.model.regression.multiple_linear_regression import (
+    MultipleLinearRegression
+)
 from autoop.core.ml.metric import MeanSquaredError
 
 
@@ -27,11 +29,11 @@ class TestPipeline(unittest.TestCase):
         self.pipeline = Pipeline(
             dataset=self.dataset,
             model=MultipleLinearRegression(),
-            input_features=list(filter(
+            input_ftrs=list(filter(
                 lambda x: x.name != "age", self.features
                 )
             ),
-            target_feature=Feature(name="age", type="numerical"),
+            target_ftr=Feature(name="age", type="numerical"),
             metrics=[MeanSquaredError()],
             split=0.8,
         )
